94069f17c4af3b403c7fc8f14684677bd6e4324d
BDCMarley <bdcmarley@protonmail.ch>
2023-06-13 17:08:17 +0200

    first commit

---
+++ ERC1155/Otl.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.19;
+
+import "./ERC1155.sol";
+
+contract ERC1155 is ERC1155 {
+    // Mapping from token ID to account balances
+    mapping(uint256 => mapping(address => uint256)) private _balances;
+
+    // Mapping from account to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    // Used as the URI for all token types by relying on ID substitution, e.g. https://token-cdn-domain/{id}.json
+    string private _uri;
+
+    /**
+     * @dev See {_setURI}.
+     */
+    constructor(string memory uri_) {
+        _setURI(uri_);
+    }
+}
